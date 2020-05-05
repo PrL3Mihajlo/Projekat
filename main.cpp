15ba84652f4e9e2436af755ced7edaecae25b201
PrL3Mihajlo <mihajlomalinovic0@gmail.com>
2020-05-05 20:35:19 +0200

    d

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+#include <Firma.hpp>
+#include <Radnici.hpp>
+#include <Vlasnik.hpp>
+#include <Cokolada.hpp>
+#include <Sirovine.hpp>
+#include <Isporuka.hpp>
+#include <Magacin.hpp>
+#include <Masina.hpp>
+#include <Menager.hpp>
+#include <Ambalaza.hpp>
+#include <Reklama.hpp>
+//Profesorka ne znam iz kog razloga mi ne rade includovi u main-u izbacuje mi gresku kao da ne postoje fajlove i ne radi mi kad ih ubacim u main
+
 void citajTxt(string nazivFajla)
 {
     string linija;
@@ -17,12 +30,14 @@ void citajTxt(string nazivFajla)
     else
         cout << "Neuspesno otvoren fajl";
 
+
 }
+
 
 int main()
 {
     citajTxt("radnici.txt");
-    cout << "Proba projekta" << endl;
+    Radnik r;
     //Cilj mog projekta je simulacija baze podataka jedne firme.Ta simulacija ce sadrzati padajuci meni svih desavanja u firmi  takodje bazi podataka mogu da pristupe samo vlasnik i menadzer koji ce dodavati razne opcije naprimer isporuke,menjati reklame,menjati ambalazu,menjati sastojke cokalade itd...
     return 0;
 }
